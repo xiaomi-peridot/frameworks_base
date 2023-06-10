0473c9a06d16707e6836c5ad1ed3c3eac63928b6
Luofan Chen <clfbbn@gmail.com>
2023-06-10 11:29:07 +0330

    base: Implement Lineage health service
    Change-Id: I772ccf6d323c24d681aa8468bf4318c7b73bd3f5
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -292,6 +292,7 @@ import com.android.server.wm.ActivityTaskManagerService;
 import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
 import com.android.server.halcyon.display.LiveDisplayService;
+import com.android.server.halcyon.health.HealthInterfaceService;
 
 import dalvik.system.VMRuntime;
 
@@ -2681,6 +2682,10 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartLiveDisplayService");
             mSystemServiceManager.startService(LiveDisplayService.class);
+            t.traceEnd();
+
+            t.traceBegin("StartHealthService");
+            mSystemServiceManager.startService(HealthInterfaceService.class);
             t.traceEnd();
         }
 
