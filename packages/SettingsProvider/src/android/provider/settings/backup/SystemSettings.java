LeafOS-Project/android_frameworks_base

base: Introduce statusbar network traffic indicator
This is a squash of the following:

Author: Jon Haus <haus.xda@gmail.com>
Date:   Thu Jan 5 00:20:06 2017 +0100

Network Traffic [1/2]

-----
abc ezio84:
use O darkintensity api
use arrows vectors from aosp net arrows commit (thanks @Mazda-- for this find!): ezio84/abc_frameworks_base@ad71ae1
fix visual glitch on statusbar expand (arrows showing and quickly hiding again)
don't show arrows when no data
remove unit settings (KB is good)
remove interval (set to 1sec by def) and single arrow options
don't get settings values again when not needed
fix padding between arrows and speed values and expose the padding value
code cleanup
-----

Change-Id: I026f67caec69a09c0c1969a09fbe99aa6b6f0510

Author: ezio84 <brabus84@gmail.com>
Date:   Sun Dec 9 20:17:58 2018 +0100

    Move net monitor to expanded statusbar header

    Preview: https://i.ibb.co/1TCCwV8/455010636-14778.jpg

    Actually, with notch or low dpi and more statusbar icons we have not so
    much space there to show the net monitor.
    A notch can hide it and the system can now handle only a fixed max number of
    notifications and status icons, so when net monitor shows itself it can force
    the system to drop some icons to show the infamous dot.

    Let's also consider we usually check the monitor to see if the data
    transfer is working as expected and the speed is proper, so it's not
    an info we want to see always (it's also distracting when reading
    something).

    So, let's move it to expanded statusbar.
    There we have also some room to add line spacing and more info.

    Change-Id: I3fca786aec4f27c763e70d404448bd71dfaecceb

Author: SpiritCroc <dev@spiritcroc.de>
Date:   Mon Dec 24 15:03:44 2018 +0100

    Network traffic: fix indicator not hiding on lost connection

    With https://gerrit.aicp-rom.com/c/AICP/frameworks_base/+/80636
    ("Statusbar net monitor: stop the handler if screen is off"),
    connectivity status can change wit

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -100,5 +100,7 @@ public class SystemSettings {
         Settings.System.CAMERA_FLASH_NOTIFICATION,
         Settings.System.SCREEN_FLASH_NOTIFICATION,
         Settings.System.SCREEN_FLASH_NOTIFICATION_COLOR,
+        Settings.System.NETWORK_TRAFFIC_STATE,
+        Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
     };
 }

xiaomi-peridot/frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -116,7 +116,9 @@ public class SystemSettings {
                 Settings.System.NOTIFICATION_COOLDOWN_ENABLED,
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
                 Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
-                Settings.System.LOCKSCREEN_ROTATION
+                Settings.System.LOCKSCREEN_ROTATION,
+                Settings.System.NETWORK_TRAFFIC_STATE,
+                Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);